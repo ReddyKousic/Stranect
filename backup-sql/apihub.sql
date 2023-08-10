024bb07dfd8bd0998d53d9adb7b943f0fb5e8893
ReddyKousic <85285392+ReddyKousic@users.noreply.github.com>
2023-08-10 15:09:25 +0530

    final SQL F

--- backup-sql/apihub.sql
+++ backup-sql/apihub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 09, 2023 at 09:32 AM
+-- Generation Time: Aug 10, 2023 at 07:17 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -36,13 +36,6 @@ CREATE TABLE `tokens` (
   `aid` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `tokens`
---
-
-INSERT INTO `tokens` (`tokenid`, `id`, `username`, `token`, `valid`, `aid`) VALUES
-(1, 43, 'kousic2211', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtvdXNpYzIyMTEiLCJpcCI6IjEyNy4wLjAuMSIsImV4cCI6MTY5MTU2Njg1MCwidXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMTUuMC4wLjAgU2FmYXJpLzUzNy4zNiIsInNpZCI6ImYwQjkwOFg4cTBTemdteEFQNjNGRGlHU1luWFBneSJ9.79_v8wYx9yg3vOPJBYYU-b1TJRmchUIH2j3s6En39m4', 1, '4xQCg5noOLbT1k3mHMz4qYaIScViiuF6qGkTBCdzRknGfOwb4N');
-
 -- --------------------------------------------------------
 
 --
@@ -57,14 +50,6 @@ CREATE TABLE `users` (
   `sid` varchar(100) NOT NULL,
   `datetime` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `aid`, `sid`, `datetime`) VALUES
-(42, 'kousic23', '$argon2id$v=19$m=65536,t=3,p=4$wVWd8PzZheMN1Rsjf8suHw$bgH8R4f4I7Bzg48uo/fpLn34E33yqgEGAMFhCHh/f60', 'PVx6ALt6faTpvYLLcNIMyEQ7CU6fO3gIZUHoI4Xk10MXwGtURL', 'Uy378mzoIhNIF3m2735MtjtplVmfVIDSZtezbfV', 'Aug 08 2023 10:23PM'),
-(43, 'kousic2211', '$argon2id$v=19$m=65536,t=3,p=4$Omyh33cHMcG9EgnPK7MyMA$eLF1TywMGeXeQWbpuaCITu8EvB/q68ZmfOWg3VQRo+Y', '4xQCg5noOLbT1k3mHMz4qYaIScViiuF6qGkTBCdzRknGfOwb4N', 'f0B908X8q0SzgmxAP63FDiGSYnXPgy', 'Aug 09 2023 09:52AM');
 
 --
 -- Indexes for dumped tables
@@ -93,13 +78,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `tokens`
 --
 ALTER TABLE `tokens`
-  MODIFY `tokenid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `tokenid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
